c99c5ff726d5c51e20e0cb62de639009967f920b
Minhaz Rahman <68527805+minhaz000@users.noreply.github.com>
2026-03-23 10:08:02 +0200

    checking connections

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,8 +5,9 @@ function Footer(props) {
     <>
      <footer className="footer flex justify-center py-7  bg-slate-800 text-white">
   <div className="text-center flex ">
-    Copyright © 2022 - All right reserved
-     <span className=' text-cyan-300 font-bold cursor-pointer'> <a href=""> Minhaz Rahman</a>  </span> 
+    Copyright © 2022 - All right reserved update 
+    676
+  I'm   <span className=' text-cyan-300 font-bold cursor-pointer'> <a href=""> Minhaz Rahman</a>  </span> 
   </div> 
 </footer>
     </>
